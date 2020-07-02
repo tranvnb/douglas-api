529c489500f4293321e07b38e1b42baf817c39e2
noxolica <angelokn14@gmail.com>
2020-07-01 23:24:32 -0700

    build: configure to allow use of sqlite for development environments without a database .env file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,31 @@
+const databaseInit = (env) => {
+  if (!env("DATABASE") || env("DATABASE") !== "postgres") {
+    console.log("DATABASE in .env not found/equal to 'postgres'. Using SQLite");
+    return {
+      client: "sqlite",
+      filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+    };
+  }
+
+  console.log("Using postgres");
+  return {
+    client: env("DATABASE"),
+    host: env("DATABASE_HOST"),
+    port: env.int("DATABASE_PORT"),
+    database: env("DATABASE_NAME"),
+    username: env("DATABASE_USERNAME"),
+    password: env("DATABASE_PASSWORD"),
+    ssl: env.bool("DATABASE_SSL", false),
+  };
+};
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
+      connector: "bookshelf",
+      settings: databaseInit(env),
+      options: {},
     },
   },
 });
